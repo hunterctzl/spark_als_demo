870ea4fde8cdb2dddee73088ab3a9ad1776b3c80
Le Zhang <le10@michaels.com>
2020-10-27 20:58:41 -0500

    adjust app

--- app.py
+++ app.py
@@ -1,31 +1,31 @@
-# from flask import Flask,request,jsonify
-# from flask_cors import CORS
-# from recommender.modeling.spark_prediction import results
-
-# app = Flask(__name__)
-# CORS(app)    
-
-# @app.route('/movie', methods=['GET'])
-
-# def recommend_movies():
-#     res = results(int(request.args.get('user')))
-#     return jsonify(res)
-
-# if __name__ == '__main__':
-#     port = int(os.environ.get("PORT" or 5000))
-#     app.run(host="0.0.0.0", port=port)
-
 from flask import Flask,request,jsonify
 from flask_cors import CORS
 from recommender.modeling.spark_prediction import results
 
 app = Flask(__name__)
-CORS(app) 
-        
-@app.route('/movie', methods=['GET'])
-def recommend_movies():
-        res = results(request.args.get('title'))
-        return jsonify(res)
+CORS(app)    
 
-if __name__=='__main__':
-        app.run(port = 5000, debug = True)
+@app.route('/movie', methods=['GET'])
+
+def recommend_movies():
+    res = results(int(request.args.get('user')))
+    return jsonify(res)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT" or 5000))
+    app.run(host="0.0.0.0", port=port)
+
+# from flask import Flask,request,jsonify
+# from flask_cors import CORS
+# from recommender.modeling.spark_prediction import results
+
+# app = Flask(__name__)
+# CORS(app) 
+        
+# @app.route('/movie', methods=['GET'])
+# def recommend_movies():
+#         res = results(request.args.get('title'))
+#         return jsonify(res)
+
+# if __name__=='__main__':
+#         app.run(port = 5000, debug = True)
